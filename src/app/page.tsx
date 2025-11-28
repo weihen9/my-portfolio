fee33f8493d1322ffee0ee7af51a297e58154fbd
wh <weihen905@gmail.com>
2025-11-28 23:25:49 +0800

    task: implement base responsive layout and sticky theme toggle
    - created initial site container architecture (mobile & desktop)
    - defined content display structure and spacing
    - made theme toggle fixed at top-right regardless of scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,114 @@ import { ModeToggle } from './components/ModeToggle'
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row h-screen justify-center ">
-      <div className="flex flex-col md:flex-row md:w-7/10 min-h-screen gap-6 border border-white">
-        <div className="flex flex-col w-full min-h-screen md:min-h-0 border justify-center border-blue-500">
-          <div className="flex flex-col w-full h-8/10 border border-orange-500">
-            <section className="w-full border border-green-500"></section>
-          </div>
-        </div>
-        <div className="w-full border border-purple-500"></div>
+    <div className="flex flex-col md:flex-row md:px-40 gap-6 ">
+      <div className="md:sticky md:top-0 md:w-1/2 h-screen border border-blue-500">
+        <div className="extra-bold ">testing testing 123</div>
       </div>
-      <div className="absolute top-5 right-5 hidden md:block">
+      <div className="flex-1 border border-purple-500">
+        <div className="space-y-32 p-8">
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+            pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu
+            aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper
+            vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos
+            himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
+            sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.
+            Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus
+            bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+            hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
+            inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+            convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus
+            nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
+            nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
+            tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus
+            fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer
+            nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
+            per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing
+            elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
+            integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur
+            adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
+            cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+            Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
+            malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+            sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+          </section>
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+            pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu
+            aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper
+            vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos
+            himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
+            sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.
+            Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus
+            bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+            hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
+            inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+            convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus
+            nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
+            nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
+            tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus
+            fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer
+            nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
+            per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing
+            elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
+            integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur
+            adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
+            cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+            Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
+            malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+            sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+          </section>
+          <section>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae
+            pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu
+            aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper
+            vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos
+            himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex
+            sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.
+            Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus
+            bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut
+            hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra
+            inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis
+            convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus
+            nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
+            nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium
+            tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus
+            fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer
+            nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent
+            per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing
+            elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
+            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar
+            vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia
+            integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur
+            adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id
+            cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.
+            Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl
+            malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti
+            sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+          </section>
+        </div>
+      </div>
+      <div className="fixed top-5 right-5 hidden md:block">
         <ModeToggle />
       </div>
     </div>
