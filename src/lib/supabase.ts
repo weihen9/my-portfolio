a67c4bd9a0429e63741adfa39ee575dffe6314fc
wh <weihen905@gmail.com>
2025-12-04 20:53:59 +0800

    fix: add .env.production for build variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vwidibeiromcxvjhjkzq.supabase.co'
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3aWRpYmVpcm9tY3h2amhqa3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3MzM3NjksImV4cCI6MjA4MDMwOTc2OX0.7YM53936B_H4BnD8PipBK3ryVADsDBnCAkoVs1U9qRQ'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
